bb2dc5c0f7181f76794424c4c42dcb3ed6cd95f3
Nathan Sonnenberg <ntsonnenberg@gmail.com>
2024-10-14 12:54:58 -0600

    Updated company and product cards on respective pages.

--- src/components/CompanyCard.tsx
+++ src/components/CompanyCard.tsx
@@ -1,32 +1,37 @@
 import { Vendor } from "@/models/Vendor";
-import { Avatar, Box } from "@mui/material";
+import {
+  Avatar,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 
 interface Props {
   vendor: Vendor;
 }
 
-const boxClasses = {
-  boxShadow: 3,
-  padding: 2,
-  borderRadius: "5px",
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  minWidth: "10em",
-};
-
 export default function CompanyCard({ vendor }: Props) {
   return (
     <Grid>
-      <Box sx={boxClasses}>
-        <Avatar
-          src={vendor.image}
-          alt="vendor-image"
-          sx={{ width: 80, height: 80 }}
-        />
-        <h4>{vendor.name}</h4>
-      </Box>
+      <Card variant="outlined" sx={{ cursor: "pointer" }}>
+        <CardContent>
+          <Avatar
+            src={vendor.image}
+            alt="vendor-image"
+            sx={{
+              width: 80,
+              height: 80,
+              display: "flex",
+              justifyContent: "center",
+            }}
+          />
+          <Typography variant="h5" component="div" sx={{ marginTop: "1em" }}>
+            {vendor.name}
+          </Typography>
+        </CardContent>
+      </Card>
     </Grid>
   );
 }
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,43 +1,33 @@
 import { Product } from "@/models/Product";
-import { Box } from "@mui/material";
-import Image from "next/image";
+import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 
 interface Props {
   product: Product;
 }
 
-const boxClasses = {
-  "box-shadow": "0 0 0.25rem grey",
-  border: "solid",
-  "border-width": "0.5px",
-  "border-color": "grey",
-  "border-radius": "5px",
-  cursor: "pointer",
-};
-
-const imageClasses = {
-  "border-radius": "5px 5px 0px 0px",
-};
-
 export default function ProductCard({ product }: Props) {
   return (
     <Grid>
-      <Box sx={boxClasses}>
-        {product?.images.length && (
-          <Image
-            src={product.images[0]}
-            alt="primary-product-image"
-            width={270}
-            height={270}
-            style={imageClasses}
+      <Card variant="outlined" sx={{ cursor: "pointer" }}>
+        {product?.images.length ? (
+          <CardMedia
+            sx={{ height: 270, width: 270 }}
+            image={product.images[0]}
+            title="Primary Product"
           />
-        )}
-        <Box sx={{ display: "flex", justifyContent: "space-around" }}>
-          <h3>{product.title}</h3>
-          <h3>${product.price}</h3>
-        </Box>
-      </Box>
+        ) : null}
+        <CardContent>
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Typography gutterBottom component="div" variant="h2">
+              {product.title}
+            </Typography>
+            <Typography gutterBottom component="div" variant="h2">
+              ${product.price}
+            </Typography>
+          </Box>
+        </CardContent>
+      </Card>
     </Grid>
   );
 }
